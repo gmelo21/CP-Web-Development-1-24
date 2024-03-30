db3658b9327017ac286199edac37af50ff5b5213
Melo <guilhermecunhamelo2105@gmail.com>
2024-03-29 23:32:57 -0300

    Exercício 3 corrigido. - João Henrique Yamamoto - https://github.com/jyp77

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ let y = 8;
 console.log(x != y);
 console.log(x == y);
 console.log(x >= y);
+
+// Exercício 3.
+
+function calcularIMC() {
+  return parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
+}
+
+
+function verificarPeso() {
+  if (imc < 18.5) {
+    return "Abaixo do peso.";
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    return "Peso ideal.";
+  } else {
+    return "Acima do peso.";
+  }
+}
+
+
+let peso = prompt("Digite seu peso: ");
+let altura = prompt("Digite sua altura: ");
+
+
+let imc = calcularIMC();
+let statusPeso = verificarPeso();
+
+
+console.log("Seu IMC é: ", imc.toFixed(2));
+console.log("Você está ", statusPeso);
